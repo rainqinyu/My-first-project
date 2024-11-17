c735206c4752323213a0f9b2dc0b8165867dd131
Kui Liang <kui.liang@bytedance.com>
2024-11-17 15:55:47 +0800

    fix

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,15 +1,16 @@
-import {useParams} from 'react-router-dom'
-import {Content} from './Content';
-import {ReviewList} from './reviewList';
-import {Nav} from './Nav';
+import { useParams } from "react-router-dom";
+import { Content } from "./Content";
+import { ReviewList } from "./ReviewList";
+import { Nav } from "./Nav";
 export const Review = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   // const item = dataList.find((a)=>(a.id === parseInt(id)))
   return (
     <div>
-      <Nav/>
-      <Content id={id}/>
-      <ReviewList/>
+      <Nav />
+      <Content id={id} />
+      <ReviewList />
     </div>
-  )
-}
+  );
+};
+
